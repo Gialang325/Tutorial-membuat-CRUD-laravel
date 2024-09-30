eb6a832a6b10011c57084d9ccec2e43ce7007520
Muh Gilang <167087015+Gialang325@users.noreply.github.com>
2024-09-30 14:58:16 +0800

    web.php

--- web.php
+++ web.php
@@ -1,16 +1,16 @@
 <?php
 // Tempatkan file kode ini di dalam folder projek-laravel\Routes
-use App\Http\Controllers\projekcontroller;
+use App\Http\Controllers\ProjekController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
-Route::get('/projek', [projekcontroller::class, 'index'])->name('projek.index');
-Route::get('/projek/create', [projekcontroller::class, 'create'])->name('projek.create');
-Route::post('/projek', [projekcontroller::class, 'store'])->name('projek.store');
-Route::get('/projek/edit/{id}', [projekcontroller::class, 'edit'])->name('projek.edit');
-Route::put('/projek/{id}', [projekcontroller::class, 'update'])->name('projek.update');
-Route::delete('/projek/{id}', [projekcontroller::class, 'destroy'])->name('projek.destroy');
+Route::get('/projek', [ProjekController::class, 'index'])->name('projek.index');
+Route::get('/projek/create', [ProjekController::class, 'create'])->name('projek.create');
+Route::post('/projek', [ProjekController::class, 'store'])->name('projek.store');
+Route::get('/projek/edit/{id}', [ProjekController::class, 'edit'])->name('projek.edit');
+Route::put('/projek/{id}', [ProjekController::class, 'update'])->name('projek.update');
+Route::delete('/projek/{id}', [ProjekController::class, 'destroy'])->name('projek.destroy');
 
